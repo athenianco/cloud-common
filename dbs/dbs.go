c623c163937f4a1283911c2598758bcea0358ee2
Denys Smirnov <dennwc@pm.me>
2020-08-07 17:20:27 +0300

    add a few common db interfaces

--- dbs/dbs.go
+++ dbs/dbs.go
@@ -1,5 +1,4 @@
 // Package dbs contains shared code for DB implementations.
-
 package dbs
 
 import "errors"
@@ -8,3 +7,15 @@ var (
 	// ErrNotFound is returned when a DB record was not found.
 	ErrNotFound = errors.New("db: not found")
 )
+
+// IteratorBase contains methods common to DB result set iterators.
+type IteratorBase interface {
+	Next() bool
+	Err() error
+	Close() error
+}
+
+// Scanner is a common interface for a row scanners.
+type Scanner interface {
+	Scan(args ...interface{}) error
+}
