74d6447681572e5fe72b75f785290ffc5cb2b729
Oleksandr Chabaiev <lwsanty@gmail.com>
2020-09-17 13:09:43 +0300

    refactor pkeyprocessor and pkeyprovider (#5)
    * refactor pkeyprocessor and pkeyprovider
    
    related to https://github.com/athenianco/metadata/pull/249
    related to [[DEV-859]]

--- pkey/gcs/gcs.go
+++ pkey/gcs/gcs.go
@@ -12,32 +12,32 @@ import (
 	"github.com/athenianco/cloud-common/pkey"
 )
 
-type GCS struct {
+type Provider struct {
 	bucket string
 	cli    *storage.Client
 }
 
-func NewFromEnv(ctx context.Context) (pkey.PKeyProvider, error) {
+func NewProviderFromEnv(ctx context.Context) (pkey.Provider, error) {
 	bucket := os.Getenv("GOOGLE_KMS_BUCKET")
 	if bucket == "" {
 		return nil, errors.New("GOOGLE_KMS_BUCKET was not set")
 	}
-	return New(ctx, bucket)
+	return NewProvider(ctx, bucket)
 }
 
-func New(ctx context.Context, bucket string) (pkey.PKeyProvider, error) {
+func NewProvider(ctx context.Context, bucket string) (pkey.Provider, error) {
 	storageCli, err := storage.NewClient(ctx)
 	if err != nil {
 		return nil, err
 	}
-	return &GCS{
+	return &Provider{
 		bucket: bucket,
 		cli:    storageCli,
 	}, nil
 }
 
-func (g *GCS) GetPrivateKeyData(ctx context.Context, appId int64) ([]byte, error) {
-	b := g.cli.Bucket(g.bucket)
+func (p *Provider) GetPrivateKeyData(ctx context.Context, appId int64) ([]byte, error) {
+	b := p.cli.Bucket(p.bucket)
 	obj := b.Object(strconv.Itoa(int(appId)))
 	r, err := obj.NewReader(ctx)
 	if err == storage.ErrBucketNotExist || err == storage.ErrObjectNotExist {
@@ -50,4 +50,42 @@ func (g *GCS) GetPrivateKeyData(ctx context.Context, appId int64) ([]byte, error
 	return ioutil.ReadAll(r)
 }
 
-func (g *GCS) Close() error { return g.cli.Close() }
+func (p *Provider) Close() error { return p.cli.Close() }
+
+type Processor struct {
+	bucket string
+	cli    *storage.Client
+}
+
+func NewProcessorFromEnv(ctx context.Context) (pkey.Processor, error) {
+	bucket := os.Getenv("GOOGLE_KMS_BUCKET")
+	if bucket == "" {
+		return nil, errors.New("GOOGLE_KMS_BUCKET was not set")
+	}
+	return NewProcessor(ctx, bucket)
+}
+
+func NewProcessor(ctx context.Context, bucket string) (pkey.Processor, error) {
+	storageCli, err := storage.NewClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &Processor{
+		bucket: bucket,
+		cli:    storageCli,
+	}, nil
+}
+
+func (p *Processor) ProcessPrivateKeyData(ctx context.Context, accID int64, data []byte) error {
+	b := p.cli.Bucket(p.bucket)
+	obj := b.Object(strconv.Itoa(int(accID)))
+	w := obj.NewWriter(ctx)
+	defer w.Close()
+
+	_, err := w.Write(data)
+	return err
+}
+
+func (p *Processor) Close() error {
+	return p.cli.Close()
+}
