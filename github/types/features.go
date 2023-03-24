e361461a16fe5e72673595b7d5f452e3ba2e1702
aleksei <aleksei@athenian.co>
2023-03-24 10:39:55 -0500

    [DEV-6224] feature types for CI disable

--- github/types/features.go
+++ github/types/features.go
@@ -4,5 +4,10 @@ const (
 	// FeatureGHE is set for accounts based on Github Enterprise instance, instead of github.com.
 	FeatureGHE = Feature("athenian.github.ghe")
 	// FeatureNoConsistency disables consistency checks for an account.
-	FeatureNoConsistency = Feature("athenian.github.no_consistency")
+	FeatureNoConsistency         = Feature("athenian.github.no_consistency")
+	FeatureNoCICheckRun          = Feature("athenian.github.drop_node_type.CheckRun")
+	FeatureNoCICheckSuite        = Feature("athenian.github.drop_node_type.CheckSuite")
+	FeatureNoCIStatus            = Feature("athenian.github.drop_node_type.Status")
+	FeatureNoCIStatusCheckRollup = Feature("athenian.github.drop_node_type.StatusCheckRollup")
+	FeatureNoCIStatusContext     = Feature("athenian.github.drop_node_type.StatusContext")
 )
